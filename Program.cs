70db9a3a0dac5b24edc7ddae2472e02409ae6a8a
Felipe Santos <felipesotnas2004@gmail.com>
2026-02-23 12:07:06 -0300

    First commit - initial structure of subscription platform system

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MiniSistemaGerenciarAssinatura
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
